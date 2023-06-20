617f139bae216322f7b59d7da166af1eb1ee0dda
bkleyner <benkleyner@gmail.com>
2023-06-20 10:21:59 -0400

    Initial Commit

---
+++ Footy Tic-Tac-Toe/Footy_Tic_Tac_ToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Footy_Tic_Tac_ToeApp.swift
+//  Footy Tic-Tac-Toe
+//
+//  Created by Benjamin Kleyner on 6/20/23.
+//
+
+import SwiftUI
+
+@main
+struct Footy_Tic_Tac_ToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
